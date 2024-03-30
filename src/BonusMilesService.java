a50003ca4877db8e7272bad239282d2c4cd0aa63
ElenaBer77 <pei281277@gmail.com>
2024-03-30 09:58:35 +0300

    correctedInlet

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
-    public int calculate(int price, int onMil) {
+    public int calculate(int price) {
         int result;
-        result = price / onMil;
+        result = price / 20;
         return result;
     }
 }
